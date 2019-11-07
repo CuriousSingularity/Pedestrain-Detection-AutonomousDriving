bcb7c04e147305e57d0ba7df2f3260696300157c
CuriousSingularity <r.bharath.ec@gmail.com>
2019-11-07 21:52:15 +0100

    Implemented Thread class for thread creation

--- OS/inc/CThread.h
+++ OS/inc/CThread.h
@@ -1,43 +1,108 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CTHREAD.H
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CThread.h
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Thread creation and handling for the process.
+ *
+ ****************************************************************************/
+
 
 #ifndef CTHREAD_H
 #define CTHREAD_H
 
-#include "CSystemResource.h"
+//System Include Files
 #include <pthread.h>
+
+//Own Include Files
+#include "./OS/inc/CSystemResource.h"
 
 class CThread {
 public:
 
-    typedef void*(*start_routine_t)(void *);
+	/**
+	 * @brief : Typedef for the start routine of a thread 
+	 *
+	 * @param : void * arguments - POSIX specific
+	 *
+	 * @return : void * - thread join
+	 */
+	typedef void*(*start_routine_t)(void *);
 
-    CThread(int32_t threadIndex, pthread_attr_t *pAttr, CSystemResource *pSysRes = NULL);
+	/**
+	 * @brief : Constructor
+	 *
+	 * @param threadIndex		: Index for a thread
+	 * @param pAttr			: Attributes
+	 * @param pSysRes		: Reference to the system resource which can be used by the threads
+	 */
+	CThread(int32_t threadIndex, pthread_attr_t *pAttr, start_routine_t thread_entry, CSystemResource *pSysRes = NULL);
 
-    virtual ~CThread();
+	/**
+	 * @brief : Destructor
+	 */
+	virtual ~CThread();
 
-    int32_t getThreadIndex();
+	/**
+	 * @brief : Get the Thread Index
+	 *
+	 * @return - Thread Index
+	 */
+	int32_t getThreadIndex();
 
-    virtual void * run(void * arg) =0;
+	/**
+	 * @brief : Pure virtual entry function for each thread
+	 *
+	 * @param arg			: POSIX specific
+	 *
+	 * @return 			: POSIX specific (check man page for more information :D )
+	 */
+	virtual void *run(void * arg) =0;
 
-    virtual CThread::start_routine_t getPointerToStartRoutine();
+	/**
+	 * @brief : Function which creates a thread based on the entry function provided
+	 *
+	 * @return : status of the thread creation
+	 */
+	global::RC_t create();
+
+	/**
+	 * @brief : Get Pointer to the start routine for the thread
+	 *
+	 * @return : Pointer to start routine
+	 */
+	virtual CThread::start_routine_t getPointerToStartRoutine();
 
 private:
-    pthread_t m_threadId;
-    pthread_attr_t m_attr;
-    start_routine_t m_thread_entry;
-    int32_t m_threadIndex;
-    CSystemResource *m_pSysRes;
+
+	/**
+	 * @brief : Thread ID - unique for a thread in a process
+	 */
+	pthread_t m_threadId;
+
+
+	/**
+	 * @brief : Thread attributes such as priority, detachable, stack, etc
+	 */
+	pthread_attr_t *m_pAttr;
+
+	/**
+	 * @brief : entry function for a thread
+	 */
+	start_routine_t m_thread_entry;
+
+	/**
+	 * @brief : Thread index to monitor the threads
+	 */
+	int32_t m_threadIndex;
+
+	/**
+	 * @brief : Pointer reference for the System resource
+	 */
+	CSystemResource *m_pSysRes;
 
 };
 /********************
-**  CLASS END
-*********************/
+ **  CLASS END
+ *********************/
 #endif /* CTHREAD_H */
--- OS/src/CThread.cpp
+++ OS/src/CThread.cpp
@@ -1,28 +1,96 @@
-#include "CThread.h"
-
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CTHREAD.CPP
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CThread.cpp
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Thread creation and handling for the process.
+ *
+ ****************************************************************************/
 
 
 //System Include Files
-
+#include <iostream>
 
 //Own Include Files
+#include "./OS/inc/CThread.h"
+
+//Namespace
+using namespace std;
+using namespace global;
 
 //Method Implementations
+/**
+ * @brief : Constructor
+ *
+ * @param threadIndex		: Index for a thread
+ * @param pAttr			: Attributes
+ * @param pSysRes		: Reference to the system resource which can be used by the threads
+ */
+CThread::CThread(int32_t threadIndex, pthread_attr_t *pAttr, start_routine_t thread_entry, CSystemResource *pSysRes)
+{
+	// if this is NULL, then the thread is create with default configuration
+	// TODO: May explore the attributes to utilize CPU and optimizate for a better 
+	// performance. Refer man pages in linux for more information :D
+	// this->m_pAttr			= pAttr;
+	this->m_pAttr			= 0;
+	this->m_threadIndex		= threadIndex;
+	this->m_threadId		= 0;
+	this->m_pSysRes			= pSysRes;
+	this->m_thread_entry		= thread_entry;
+}
 
-int32_t CThread::getThreadIndex(){}
-CThread::CThread(int32_t threadIndex, pthread_attr_t *pAttr, CSystemResource *pSysRes){}
-CThread::~CThread(){}
+
+/**
+ * @brief : Destructor
+ */
+CThread::~CThread()
+{
+	// Disconnect from the global resource
+	this->m_pSysRes = 0;
+}
+
+
+/**
+ * @brief : Get Pointer to the start routine for the thread
+ *
+ * @return : Pointer to start routine
+ */
 CThread::start_routine_t CThread::getPointerToStartRoutine()
 {
-	return NULL;
+	return this->m_thread_entry;
+}
+
+
+/**
+ * @brief : Get the Thread Index
+ *
+ * @return - Thread Index
+ */
+int32_t CThread::getThreadIndex()
+{
+	return this->m_threadIndex;
+}
+
+
+/**
+ * @brief : Function which creates a thread based on the entry function provided
+ *
+ * @return : status of the thread creation
+ */
+RC_t CThread::create()
+{
+	if (this->m_thread_entry == 0)
+	{
+		return RC_ERROR_NULL;
+	}
+
+	// Thread parameter arguments is made NULL since it is redandent
+	// TODO: If parameter is needed for each threads independently, explore this API options
+	if (pthread_create(&this->m_threadId, this->m_pAttr, this->m_thread_entry, NULL) != 0)
+	{
+		cout << "ERROR\t: Thread " << this->m_threadIndex << " creation failed with errno " << errno << endl;
+	}
+
+	return RC_SUCCESS;
 }
 
