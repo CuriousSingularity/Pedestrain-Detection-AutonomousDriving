0fcb5c479b050dfa8d15c071fdd39230b515720f
CuriousSingularity <r.bharath.ec@gmail.com>
2019-11-07 23:11:47 +0100

    Design using Together Tool added
    + Design creation added
    + Script to update design

---
+++ generate_design.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Design files
+DESIGN_PATH=./design
+
+# create a directory for design 
+mkdir -p $DESIGN_PATH
+
+# remove the old files
+rm $DESIGN_PATH/*.cpp
+rm $DESIGN_PATH/*.h
+
+# gather all the files for the design
+find . -name "*.cpp" -exec cp -f -t $DESIGN_PATH {} +
+find . -name   "*.h" -exec cp -f -t $DESIGN_PATH {} +
