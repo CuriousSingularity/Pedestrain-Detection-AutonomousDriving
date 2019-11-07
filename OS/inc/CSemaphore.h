f2f9f6e320f1ea49f123b0d739187a17d3cd7b93
CuriousSingularity <r.bharath.ec@gmail.com>
2019-11-07 21:34:10 +0100

    Semaphore for synchronization
    + Implemented Semaphore class
    + Comments added

--- OS/inc/CSemaphore.h
+++ OS/inc/CSemaphore.h
@@ -1,46 +1,108 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CSEMAPHORE.H
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CSemaphore.h
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Semaphore implementation for synchronisation between 
+ * 			threads or thread and resource
+ *
+ ****************************************************************************/
+
 
 #ifndef CSEMAPHORE_H
 #define CSEMAPHORE_H
 
+//System Include Files
 #include <string>
+#include <semaphore.h>
+
+//Own Include Files
+#include "./global.h"
 
 class CSemaphore {
 private:
 
-    void open();
+	/**
+	 * @brief : Semaphore name
+	 */
+	std::string m_name;
 
-    void close();
+	/**
+	 * @brief : Semaphore mode - between threads of a process / between processes
+	 */
+	int m_mode;
+
+	/**
+	 * @brief : Initial value of the semaphore
+	 */
+	unsigned int m_value;
+
+	/**
+	 * @brief : semaphore
+	 */
+	sem_t m_sem;
+
+	/**
+	 * @brief : Initialises the semaphore
+	 *
+	 * @return RC_t - status of initialisation
+	 */
+	global::RC_t init();
+
+	/**
+	 * @brief : Destroys the semaphore
+	 *
+	 * @return RC_t - status of destruction
+	 */
+	global::RC_t destroy();
 
 public:
 
-    CSemaphore(std::string name, int oflag, mode_t mode, unsigned int arg);
+	/**
+	 * @brief : Constructor
+	 *
+	 * @param name		: name of the semaphore
+	 * @param mode		: mode of the semaphore
+	 * @param value		: inital value of the semaphore
+	 */
+	CSemaphore(std::string name, int mode, unsigned int value = 1);
 
-    ~CSemaphore();
+	/**
+	 * @brief : Destructor
+	 */
+	~CSemaphore();
 
-    int wait();
+	/**
+	 * @brief : Blocking wait until event
+	 *
+	 * @return RC_t - status
+	 */
+	global::RC_t wait();
 
-    int trywait();
+	/**
+	 * @brief : Non-Blocking wait until event
+	 *
+	 * @return RC_t - status
+	 */
+	global::RC_t trywait();
 
-    int timedwait();
+	/**
+	 * @brief : Timed-Blocking wait until event
+	 *
+	 * @param abs_timeout	: absolute timeperiod 
+	 *
+	 * @return RC_t - status
+	 */
+	global::RC_t timedwait(const struct timespec &abs_timeout);
 
-    int post();
-
-private:
-    std::string m_name;
-    int oflag;
-    mode_t mode;
-    unsigned int value;
+	/**
+	 * @brief : Set event to wake up the blocked threads
+	 *
+	 * @return RC_t - status
+	 */
+	global::RC_t post();
 };
 /********************
-**  CLASS END
-*********************/
+ **  CLASS END
+ *********************/
 #endif /* CSEMAPHORE_H */
--- OS/src/CSemaphore.cpp
+++ OS/src/CSemaphore.cpp
@@ -1,29 +1,170 @@
-#include "CSemaphore.h"
+/***************************************************************************
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CSemaphore.cpp
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Semaphore implementation for synchronisation between 
+ * 			threads or thread and resource
+ *
+ ****************************************************************************/
 
 //System Include Files
-
+#include <iostream>
 
 //Own Include Files
+#include "./OS/inc/CSemaphore.h"
+
+//Namespaces
+using namespace std;
+using namespace global;
 
 //Method Implementations
+/**
+ * @brief : Constructor
+ *
+ * @param name		: name of the semaphore
+ * @param mode		: mode of the semaphore
+ * @param value		: inital value of the semaphore
+ */
+CSemaphore::CSemaphore(std::string name, int mode, unsigned int value)
+{
+	this->m_name	= name;
+	this->m_mode	= mode;
+	this->m_value	= value;
+
+	if (this->init() != RC_SUCCESS)
+	{
+		cout << "ERROR\t: Failed to create a semaphore " << this->m_name << endl;
+	}
+	else
+	{
+		cout << "INFO\t: Create semaphore " << this->m_name << endl;
+	}
+}
 
 
-int CSemaphore::wait(){}
-int CSemaphore::trywait(){}
-int CSemaphore::timedwait(){}
-int CSemaphore::post(){}
+/**
+ * @brief : Destructor
+ */
+CSemaphore::~CSemaphore()
+{
+	if (this->destroy() != RC_SUCCESS)
+	{
+		cout << "ERROR\t: Failed to destroy a semaphore " << this->m_name << endl;
+	}
+	else
+	{
+		cout << "INFO\t: Destroy semaphore " << this->m_name << endl;
+	}
+}
 
-CSemaphore::~CSemaphore(){}
 
-/***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CSEMAPHORE.CPP
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
-void CSemaphore::open(){}
-void CSemaphore::close(){}
-CSemaphore::CSemaphore(std::string name, int oflag, mode_t mode, unsigned int arg){}
+/**
+ * @brief : Initialises the semaphore
+ *
+ * @return RC_t - status of initialisation
+ */
+RC_t CSemaphore::init()
+{
+	RC_t ret = RC_ERROR;
+
+	if (::sem_init(&this->m_sem, this->m_mode, this->m_value) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+}
+
+/**
+ * @brief : Destroys the semaphore
+ *
+ * @return RC_t - status of destruction
+ */
+RC_t CSemaphore::destroy()
+{
+	RC_t ret = RC_ERROR;
+
+	if (::sem_destroy(&this->m_sem) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+}
+
+
+/**
+ * @brief : Blocking wait until event
+ *
+ * @return RC_t - status
+ */
+RC_t CSemaphore::wait()
+{
+	RC_t ret = RC_ERROR;
+
+	if (::sem_wait(&this->m_sem) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+}
+
+
+/**
+ * @brief : Non-Blocking wait until event
+ *
+ * @return RC_t - status
+ */
+RC_t CSemaphore::trywait()
+{
+	RC_t ret = RC_ERROR;
+
+	if (::sem_trywait(&this->m_sem) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+}
+
+
+/**
+ * @brief : Timed-Blocking wait until event
+ *
+ * @param abs_timeout	: absolute timeperiod 
+ *
+ * @return RC_t - status
+ */
+RC_t CSemaphore::timedwait(const struct timespec &abs_timeout)
+{
+	RC_t ret = RC_ERROR_TIME_OUT;
+
+	if (::sem_timedwait(&this->m_sem, &abs_timeout) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+
+
+}
+
+/**
+ * @brief : Set event to wake up the blocked threads
+ *
+ * @return RC_t - status
+ */
+RC_t CSemaphore::post()
+{
+	RC_t ret = RC_ERROR;
+
+	if (::sem_post(&this->m_sem) == 0)
+	{
+		ret = RC_SUCCESS;
+	}
+
+	return ret;
+}
+
