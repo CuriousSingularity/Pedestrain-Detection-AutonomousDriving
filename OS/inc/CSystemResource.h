c7182938bbaf38b98371a47043648939ea408fc3
CuriousSingularity <r.bharath.ec@gmail.com>
2019-11-07 21:50:36 +0100

    System resource class implemented

--- OS/inc/CSystemResource.h
+++ OS/inc/CSystemResource.h
@@ -1,31 +1,64 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CSYSTEMRESOURCE.H
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CSystemResource.h
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : System resource is a collection of all the resource used
+ * 			for the particular application.
+ *
+ ****************************************************************************/
+
 
 #ifndef CSYSTEMRESOURCE_H
 #define CSYSTEMRESOURCE_H
-#include "CCom.h"
-#include "CCamera.h"
+
+//System Include Files
+#include <string>
+
+//Own Include Files
+#include "./HAL/inc/CCom.h"
+#include "./HAL/inc/CCamera.h"
+
 class CSystemResource {
 private:
-    CCom m_serial;
-    CCamera m_camera;
+
+	/**
+	 * @brief : UART Class
+	 */
+	CCom m_serial;
+
+	/**
+	 * @brief : Camera Class
+	 */
+	CCamera m_camera;
+
 public:
-    CSystemResource(){};
+	/**
+	 * @brief : Constructor
+	 */
+	CSystemResource();
 
-    ~CSystemResource(){};
+	/**
+	 * @brief : Destructor
+	 */
+	~CSystemResource();
 
-    CCom& getSerialResourceReference();
+	/**
+	 * @brief : Get the reference to the UART device
+	 *
+	 * @return - reference to the device
+	 */
+	CCom& getSerialResourceReference();
 
-    CCamera& getCameraResourceReference();
+	/**
+	 * @brief : Get the reference to the Camera device
+	 *
+	 * @return - reference to the device
+	 */
+	CCamera& getCameraResourceReference();
+
 };
 /********************
-**  CLASS END
-*********************/
+ **  CLASS END
+ *********************/
 #endif /* CSYSTEMRESOURCE_H */
--- OS/src/CSystemResource.cpp
+++ OS/src/CSystemResource.cpp
@@ -1,24 +1,62 @@
-#include "CSystemResource.h"
-#include "CCom.h"
-#include "CCamera.h"
-
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CSYSTEMRESOURCE.CPP
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CSystemResource.cpp
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : System resource is a collection of all the resource used
+ * 			for the particular application.
+ *
+ ****************************************************************************/
 
 
 //System Include Files
 
-
 //Own Include Files
+#include "CSystemResource.h"
+#include "CCom.h"
+#include "CCamera.h"
+
+//Namespace
+using namespace std;
+using namespace global;
 
 //Method Implementations
+/**
+ * @brief : Constructor
+ */
+CSystemResource::CSystemResource()
+{
+	//nothing
+}
 
-CCom& CSystemResource::getSerialResourceReference(){}
-CCamera& CSystemResource::getCameraResourceReference(){}
+
+/**
+ * @brief : Destructor
+ */
+CSystemResource::~CSystemResource()
+{
+	//nothing
+}
+
+
+/**
+ * @brief : Get the reference to the UART device
+ *
+ * @return - reference to the device
+ */
+CCom& CSystemResource::getSerialResourceReference()
+{
+	return (this->m_serial);
+}
+
+
+/**
+ * @brief : Get the reference to the Camera device
+ *
+ * @return - reference to the device
+ */
+CCamera& CSystemResource::getCameraResourceReference()
+{
+	return (this->m_camera);
+}
+
