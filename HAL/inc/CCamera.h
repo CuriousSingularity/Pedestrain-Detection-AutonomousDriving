6f83d77d6ba544549bf9e260b3a3bb5a3b569774
CuriousSingularity <r.bharath.ec@gmail.com>
2019-11-07 21:43:22 +0100

    camera abstraction
    + Camera class implemented
    + Configuration needs to be done based on the camera and the platform - pending

--- HAL/inc/CCamera.h
+++ HAL/inc/CCamera.h
@@ -1,26 +1,53 @@
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CCAMERA.H
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CCamera.h
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Camera class to capture frame or stream the video
+ *
+ ****************************************************************************/
+
 
 #ifndef CCAMERA_H
 #define CCAMERA_H
-#include "CResource.h"
+
+//System Include Files
+
+//Own Include Files
+#include "./OS/inc/CResource.h"
+
 class CCamera : public CResource {
 private:
-    global::RC_t configure();
+
+	/**
+	 * @brief : Configure the Camera interface
+	 * 		- Framerate
+	 * 		- Resolution
+	 * 		- Orientation
+	 * 		- ISO
+	 * 		- Brightness
+	 * 		- Contrast
+	 * 		- Sharpness
+	 * 		- Satureation
+	 *
+	 * @return RC_t : Status of the camera
+	 */
+	global::RC_t configure();
 
 public:
-    CCamera();
 
-    virtual ~CCamera();
+	/**
+	 * @brief : Constructor
+	 */
+	CCamera();
+
+	/**
+	 * @brief : Destructor
+	 */
+	virtual ~CCamera();
+
 };
 /********************
-**  CLASS END
-*********************/
+ **  CLASS END
+ *********************/
 #endif /* CCAMERA_H */
--- HAL/src/CCamera.cpp
+++ HAL/src/CCamera.cpp
@@ -1,36 +1,65 @@
-#include "CCamera.h"
-#include <unistd.h>
 /***************************************************************************
-*============= Copyright by Darmstadt University of Applied Sciences =======
-****************************************************************************
-* Filename        : CCAMERA.CPP
-* Author          :
-* Description     :
-*
-*
-****************************************************************************/
-
+ *============= Copyright by Darmstadt University of Applied Sciences =======
+ ****************************************************************************
+ * Filename        : CCamera.cpp
+ * Author          : Bharath Ramachandraiah (stbhrama@stud.h-da.de)
+ * Description     : Camera class to capture frame or stream the video
+ *
+ ****************************************************************************/
 
 //System Include Files
+#include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 //Own Include Files
+#include "./HAL/inc/CCamera.h"
 
+//Namespace
+using namespace std;
 using namespace global;
 
 //Method Implementations
-
-RC_t CCamera::configure()
+/**
+ * @brief : Constructor
+ */
+CCamera::CCamera() : CResource("/dev/video0", O_RDWR | O_NOCTTY | O_SYNC, S_IRWXU) 
 {
-	return RC_ERROR;
+	if (this->configure() != RC_SUCCESS)
+	{
+		cout << "ERROR\t: Failed to configure the camera " << this->getDeviceNode() << endl;
+	}
+	else
+	{
+		cout << "INFO\t: Configured the camera " << this->getDeviceNode() << endl;
+	}
 }
 
-CCamera::CCamera() : CResource("/dev/camera", O_RDWR | O_NOCTTY | O_SYNC, S_IRWXU) 
-{
-}
 
+/**
+ * @brief : Destructor
+ */
 CCamera::~CCamera()
 {
+	// nothing
+}
+
+/**
+ * @brief : Configure the Camera interface
+ * 		- Framerate
+ * 		- Resolution
+ * 		- Orientation
+ * 		- ISO
+ * 		- Brightness
+ * 		- Contrast
+ * 		- Sharpness
+ * 		- Satureation
+ *
+ * @return RC_t : Status of the camera
+ */
+RC_t CCamera::configure()
+{
+	return RC_ERROR_INVALID_STATE;
 }
